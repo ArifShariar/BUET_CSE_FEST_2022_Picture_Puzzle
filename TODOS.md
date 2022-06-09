318d86222774cb1f21547a4b7d84ddac55affd26
Farhana100 <farhanakhanhp@gmail.com>
2022-06-09 10:57:08 +0600

    updates: - bug fixed - participant table entries saved after register - filtered home page contents as per user auth status - fetch rank list from database for register page

--- TODOS.md
+++ TODOS.md
@@ -5,4 +5,9 @@ This is a list of tasks left to implement. This list will be updated on the go.
 * add pop up messages for login, logout, and register 
     * pass from backend
 * create puzzle model in contest arena
-* filter home page contents as per user auth status
+* fetch rank list from database for register page
+
+## Updates in last version
+- bug fixed - participant table entries saved after register
+- filtered home page contents as per user auth status
+- fetch rank list from database for register page
--- contest_arena/views.py
+++ contest_arena/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from user.models import *
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    to_frontend = {
+        "user_active": request.user.is_authenticated,
+        "user": request.user,
+    }
+
+    print(to_frontend["user_active"])
+    print(to_frontend["user"])
+
+    return render(request, 'home/home.html', to_frontend)
 
 
 def view_rank_list_page(request):
-    return render(request, 'rank_list/rank_list.html')
+    if not request.user.is_authenticated:
+        return redirect('home')
+
+    participants = User.objects.filter(is_staff=False).order_by('participant__position')
+
+    rank_list = []
+    for p in participants:
+        rank_list.append(p)
+        print(p.participant.position, p.username, p.participant.student_ID)
+
+    to_frontend = {
+        "user_active": request.user.is_authenticated,
+        "user": request.user,
+        "rank_list": rank_list,
+    }
+
+    return render(request, 'rank_list/rank_list.html', to_frontend)
